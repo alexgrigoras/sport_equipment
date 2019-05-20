d53c6824df075131eec963a2998d9d1b9d8507c2
alexgrigoras <alex_grigoras_10@yahoo.com>
2019-05-20 22:54:10 +0300

    Added the SportEquipmentUnitTest project for testing the application

---
+++ MagazinSportiv/SportEquipmentUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportEquipmentUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportEquipmentUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("68b81768-1ee8-492f-bcd5-9f0673d75eb2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MagazinSportiv/SportEquipmentUnitTest/UnitTest.cs
@@ -0,0 +1,26 @@
+﻿/// <copyright file="UnitTest.cs" >
+/// Copyright (c) MagazinSportiv. All Right Reserved
+/// </copyright>
+/// <author>
+/// Bodnariu Andreea, Grigoras Alexandru, Luncanu Iuliana, Suflet Ioan
+/// </author>
+/// <summary>
+/// UnitTest class used for testing the application
+/// </summary>
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SportEquipmentUnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        /// <function>TestMethod1</function>
+        /// <summary>Tests ...</summary>
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
