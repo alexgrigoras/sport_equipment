049f7f12a71f45e1d3d1c9bb9520511ee50689a4
alexgrigoras <alex_grigoras_10@yahoo.com>
2019-05-26 13:25:52 +0300

    Added save training and modified select functions

--- MagazinSportiv/MagazinSportiv/ConcreteClasses/FitnessUser.cs
+++ MagazinSportiv/MagazinSportiv/ConcreteClasses/FitnessUser.cs
@@ -10,8 +10,10 @@
 
 #region using
 
+using LibPrinter;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,27 +28,50 @@ namespace SportEquipment
         
         public override void SelectTrainer(string trainer)
         {
-            _trainer = trainer;          
+            _trainer = "fitness-" + trainer;          
         }
 
         public override void SelectFood(string food)
         {
-            _food = food;
+            _food = "fitness-" + food;
         }
 
         public override void SelectClothes(string clothes)
         {
-            _clothes = clothes;
+            _clothes = "fitness-" + clothes;
         }
 
         public override void SelectEquipment(string equipment)
         {
-            _equipment = equipment;
+            _equipment = "fitness-" + equipment;
         }
 
         public override void PrintTraining()
         {
             // print the training data to file
+            string dateTime = DateTime.Now.ToString("dddd_dd_MMMM_yyyy_HH_mm");
+            string fileName = "workouts/workout_" + dateTime + ".txt";
+
+            Printer p = new Printer(fileName);
+
+            if (!p.OpenFile())
+            {
+                throw new Exception("Directorul sau fisierul este invalid");
+            }
+
+            p.Writeln("Fitness training\n");
+            p.Writeln("----------------");
+            p.Writeln("Fitness is the quantitative representation of natural and sexual selection within evolutionary biology. It can be defined either with respect to a genotype or to a phenotype in a given environment. In either case, it describes individual reproductive success and is equal to the average contribution to the gene pool of the next generation that is made by individuals of the specified genotype or phenotype. The fitness of a genotype is manifested through its phenotype, which is also affected by the developmental environment. The fitness of a given phenotype can also be different in different selective environments.");
+            p.Writeln("----------------\n");
+            p.Writeln("Selected workout:");
+            p.Writeln("- Trainer: " + _trainer);
+            p.Writeln("- Food: " + _food);
+            p.Writeln("- Clothes: " + _clothes);
+            p.Writeln("- Equipment: " + _equipment);
+
+            p.CloseFile();
+
+            Process.Start("notepad.exe", fileName);
         }
 
         #endregion
