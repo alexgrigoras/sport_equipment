257e5d1056aa7393d824296091678a4a9d0df7f5
alexgrigoras <alex_grigoras_10@yahoo.com>
2019-05-26 22:23:00 +0300

    Added another 10 tests for methods and interface

--- MagazinSportiv/SportEquipmentUnitTest/UnitTest.cs
+++ MagazinSportiv/SportEquipmentUnitTest/UnitTest.cs
@@ -226,8 +226,8 @@ namespace SportEquipment
             CollectionAssert.AreEqual(expectedProductListEquipment, (List<string>)realProductListEquipment);
         }
 
-        /// <function>Test_GetDataFromDB_CorrectWrongUserType</function>
-        /// <summary>Tests if the user type is wrong it displays empty collection</summary>
+        /// <function>Test_GetDataFromDB_CorrectWrongCategory</function>
+        /// <summary>Tests if the category is wrong it displays empty collection</summary>
         [TestMethod]
         public void Test_GetDataFromDB_CorrectWrongCategory()
         {
@@ -245,6 +245,197 @@ namespace SportEquipment
             List<string> expectedProductListEquipment = new List<string>();
 
             CollectionAssert.AreEqual(expectedProductListEquipment, (List<string>)realProductListEquipment);
+        }
+
+        /// <function>Test_FilePrinter_CorrectOpenFile</function>
+        /// <summary>Tests if the file is opened successfully</summary>
+        [TestMethod]
+        public void Test_FilePrinter_CorrectOpenFile()
+        {
+            bool expectedResult = true;
+            string fileName = "files/file.txt";
+            LibPrinter.Printer p = new LibPrinter.Printer(fileName);
+
+            bool realResult = p.OpenFile();
+
+            Assert.AreEqual(realResult, expectedResult);
+        }
+
+        /// <function>Test_FilePrinter_IncorrecDirectoryOpenFile</function>
+        /// <summary>Tests if the file directory is invalid</summary>
+        [TestMethod]
+        public void Test_FilePrinter_IncorrecDirectoryOpenFile()
+        {
+            bool expectedResult = true;
+            string fileName = "docs/file.txt";
+            LibPrinter.Printer p = new LibPrinter.Printer(fileName);
+
+            bool realResult = p.OpenFile();
+
+            Assert.AreEqual(realResult, expectedResult);
+        }
+
+        /// <function>Test_FilePrinter_IncorrectWriteWithoutOpenFile</function>
+        /// <summary>Tests if it writes without file opened</summary>
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void Test_FilePrinter_ExceptionWriteWithoutOpenFile()
+        {
+            string fileName = "file/file.txt";
+            LibPrinter.Printer p = new LibPrinter.Printer(fileName);
+
+            p.Write("abc");
+        }
+
+        /// <function>Test_FilePrinter_IncorrectWritelnWithoutOpenFile</function>
+        /// <summary>Tests if it writes without file opened</summary>
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void Test_FilePrinter_ExceptionWritelnWithoutOpenFile()
+        {
+            string fileName = "file/file.txt";
+            LibPrinter.Printer p = new LibPrinter.Printer(fileName);
+
+            p.Writeln("def");
+        }
+
+        /// <function>Test_FilePrinter_CorrectWriteOnFile</function>
+        /// <summary>Tests if it writes on file</summary>
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void Test_FilePrinter_CorrectWriteOnFile()
+        {
+            string fileName = "file/file.txt";
+            LibPrinter.Printer p = new LibPrinter.Printer(fileName);
+
+            p.OpenFile();
+            p.Write("abc");
+        }
+
+        /// <function>Test_FilePrinter_CorrectWritelnOnFile</function>
+        /// <summary>Tests if it writes on file</summary>
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void Test_FilePrinter_CorrectWritelnOnFile()
+        {
+            string fileName = "file/file.txt";
+            LibPrinter.Printer p = new LibPrinter.Printer(fileName);
+
+            p.OpenFile();
+            p.Writeln("def");
+        }
+
+        /// <function>Test_MainForm_ComboBoxSupliments</function>
+        /// <summary>Tests if the interface displays correct information on supliments combo box</summary>
+        [TestMethod]
+        public void Test_MainForm_ComboBoxSupliments_OneValue()
+        {
+            var userType = UserType.FitnessUser;
+            string username = "alex";
+            var loginForm = new LoginForm();
+            var mainForm = new MainForm(userType, username, loginForm);
+
+            mainForm.Show();
+
+            var privateObject = new PrivateObject(mainForm);
+
+            System.Windows.Forms.Button randomizeButton = (System.Windows.Forms.Button)privateObject.GetFieldOrProperty("randomizeButton");
+
+            randomizeButton.PerformClick();
+
+            System.Windows.Forms.ComboBox suplimentsComboBox = (System.Windows.Forms.ComboBox)privateObject.GetFieldOrProperty("suplimentsComboBox");
+
+            string realText = suplimentsComboBox.Text;
+            string expectedText = "Magneziu";
+
+            Assert.AreEqual(expectedText, realText);
+        }
+
+        /// <function>Test_MainForm_ComboBoxEquipment</function>
+        /// <summary>Tests if the interface displays correct information on equipment combo box</summary>
+        [TestMethod]
+        public void Test_MainForm_ComboBoxEquipment_MultipleValues()
+        {
+            var userType = UserType.FitnessUser;
+            string userTypeString = "Fitness";
+            string category = "Equipment";
+            string username = "alex";
+            var loginForm = new LoginForm();
+            var mainForm = new MainForm(userType, username, loginForm);
+
+            mainForm.Show();
+
+            var privateObject = new PrivateObject(mainForm);
+
+            System.Windows.Forms.Button randomizeButton = (System.Windows.Forms.Button)privateObject.GetFieldOrProperty("randomizeButton");
+
+            randomizeButton.PerformClick();
+
+            System.Windows.Forms.ComboBox equipmentComboBox = (System.Windows.Forms.ComboBox)privateObject.GetFieldOrProperty("equipmentComboBox");
+
+            List<string> realProductListEquipment = (List<string>)privateObject.Invoke("GetDataFromDB", new object[] { userTypeString, category });
+
+            string realText = equipmentComboBox.Text;
+
+            CollectionAssert.Contains(realProductListEquipment, realText);
+        }
+
+        /// <function>Test_MainForm_ComboBoxGear</function>
+        /// <summary>Tests if the interface displays correct information on gear combo box</summary>
+        [TestMethod]
+        public void Test_MainForm_ComboBoxGear()
+        {
+            var userType = UserType.FitnessUser;
+            string userTypeString = "Fitness";
+            string category = "Clothes";
+            string username = "alex";
+            var loginForm = new LoginForm();
+            var mainForm = new MainForm(userType, username, loginForm);
+
+            mainForm.Show();
+
+            var privateObject = new PrivateObject(mainForm);
+
+            System.Windows.Forms.Button randomizeButton = (System.Windows.Forms.Button)privateObject.GetFieldOrProperty("randomizeButton");
+
+            randomizeButton.PerformClick();
+
+            System.Windows.Forms.ComboBox gearComboBox = (System.Windows.Forms.ComboBox)privateObject.GetFieldOrProperty("gearComboBox");
+
+            List<string> realProductListGear = (List<string>)privateObject.Invoke("GetDataFromDB", new object[] { userTypeString, category });
+
+            string realText = gearComboBox.Text;
+
+            CollectionAssert.Contains(realProductListGear, realText);
+        }
+
+        /// <function>Test_MainForm_ComboBoxGear</function>
+        /// <summary>Tests if the interface displays correct information on trainer combo box</summary>
+        [TestMethod]
+        public void Test_MainForm_ComboBoxTrainer()
+        {
+            var userType = UserType.FitnessUser;
+            string userTypeString = "Fitness";
+            string category = "Trainer";
+            string username = "alex";
+            var loginForm = new LoginForm();
+            var mainForm = new MainForm(userType, username, loginForm);
+
+            mainForm.Show();
+
+            var privateObject = new PrivateObject(mainForm);
+
+            System.Windows.Forms.Button randomizeButton = (System.Windows.Forms.Button)privateObject.GetFieldOrProperty("randomizeButton");
+
+            randomizeButton.PerformClick();
+
+            System.Windows.Forms.ComboBox trainerComboBox = (System.Windows.Forms.ComboBox)privateObject.GetFieldOrProperty("trainerComboBox");
+
+            List<string> realProductListTrainer = (List<string>)privateObject.Invoke("GetDataFromDB", new object[] { userTypeString, category });
+
+            string realText = trainerComboBox.Text;
+
+            CollectionAssert.Contains(realProductListTrainer, realText);
         }
     }
 }
